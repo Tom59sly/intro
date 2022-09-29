f6a7d7e2c282764b9fda812db7f40fe69aa3aeaf
Tom59sly <tom.desrousseaux@live.fr>
2022-09-29 10:36:03 +0200

    Exercices 1-5

--- src/main/java/fr/dampierre/App.java
+++ src/main/java/fr/dampierre/App.java
@@ -1,11 +1,12 @@
 package fr.dampierre;
 
+import java.util.Random;
 import java.util.Scanner;
 
 public class App {
     
-    public static void main(String[] args) {       
-        System.out.println("Bonjour");
+    public static void main(String[] args) {
+        System.out.println("Bonjour");  
 
         // Récupérer le nombre d'heures travaillées
         // int heureTravaillees = 120;
@@ -25,22 +26,103 @@ public class App {
         // // Afficher le résulltat
         // System.out.println("Le salaire brut est de : " + salaireBrut);
 
-        Scanner clavier = new Scanner(System.in);
-        System.out.println("Saisir un nom commun");
-        String nomCommun1 = clavier.next();
 
-        System.out.println("Saisir un nombre");
-        double nombre = clavier.nextDouble();
+        // COURS 
 
-        System.out.println("Saisir le deuxieme nom commun");
-        String nomCommun2 = clavier.next();
 
-        System.out.println("Saisir un adjectif");
-        String adjectif = clavier.next();
+        // Scanner clavier = new Scanner(System.in);
+        // System.out.println("Saisir un nom commun");
+        // String mot1 = clavier.nextLine();
+
+        // System.out.println("Saisir un nombre");
+        // int nombre = clavier.nextInt();
+        // clavier.nextLine();
+
+        // System.out.println("Saisir le deuxieme nom commun");
+        // String mot2 = clavier.nextLine();
+
+        // System.out.println("Saisir un adjectif");
+        // String adjectif = clavier.nextLine();
         
-        clavier.close();
+        // clavier.close();
 
-        System.out.println("Il était une fois un " + nomCommun1 + " qui avait aussi " + nombre + " " + nomCommun2 + ". Cela le rendait très " + adjectif+".");
+        // String phrase = "Il était une fois un " + mot1 + " qui avait aussi " + nombre + " " + mot2 + ". Cela le rendait très " + adjectif + ".";
 
-        }
+        // System.out.println(phrase);
+
+
+        // Exercice 1
+
+        int heure = 12;
+        int minutes = 34;
+        int secondes = 56;
+        int resSecondes = heure * 3600 + minutes * 60 + secondes;
+        System.out.println("Il s'est écoulé " + resSecondes +" secondes depuis minuit.");
+
+        // Exercice 2
+
+        Scanner keyboard = new Scanner(System.in);
+        System.out.println("Entrer les heures :");
+        int heure1 = keyboard.nextInt();
+
+        System.out.println("Entrer les minutes :");
+        int minutes1 = keyboard.nextInt();
+
+        System.out.println("Entrer les secondes :");
+        int secondes1 = keyboard.nextInt();
+        keyboard.close();
+
+        int resSecondes1 = heure1 * 3600 + minutes1 * 60 + secondes1;
+        System.out.println("Il s'est écoulé " + resSecondes1 +" secondes depuis minuit.");
+
+        // Exercice 3
+
+        // Scanner keyboard = new Scanner(System.in);
+        // System.out.println("Entrer les heures :");
+        // int heure = keyboard.nextInt();
+
+        // System.out.println("Entrer les minutes :");
+        // int minutes = keyboard.nextInt();
+
+        // System.out.println("Entrer les secondes :");
+        // int secondes = keyboard.nextInt();
+        // keyboard.close();
+
+        // int resSecondes = 24*3600 - (heure * 3600 + minutes * 60 + secondes);
+        // System.out.println("Il y a encore " + resSecondes +" secondes avant minuit.");
+        
+        // Exercice 4
+
+        // int secondesDebut = 45296;
+        // int secondesFin = 15 * 3600 + 24*60 + 12;
+
+        // double pourcentage = (secondesFin - secondesDebut);
+        // pourcentage = pourcentage / 86400;
+
+        // System.out.println(pourcentage * 100);
+
+        // Exercice 5
+        
+        // System.out.println("Je pense à un nombre entre 1 et 100 inclus. Devinez lequel.");
+        // Random random = new Random();
+        // int nb = random.nextInt(101);
+        
+        // Scanner clavier = new Scanner(System.in);
+
+        // System.out.println("Entrez un nombre : ");
+        // int NombreUtilisateur = clavier.nextInt();
+        // clavier.close();
+
+        // System.out.println("Vous proposez : " + NombreUtilisateur);
+        // System.out.println("Le nombre auquel je pensais était : " + nb);
+
+        // if (NombreUtilisateur == nb) {
+        //     System.out.println("Vous avez trouvé le bon nombre !");
+        // }
+        // else {
+            
+        //     System.out.println("Vous étiez à "+ Math.abs(nb - NombreUtilisateur) + " de la bonne réponse.");
+        // }
+        
     }
+}
